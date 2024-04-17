8d68bc30723ad09457307fcac8289cc151fd642a
Harshit-Jajoria <harshitjajoria66@gmail.com>
2024-04-17 11:39:45 +0530

    first commit

---
+++ frontend/src/pages/ProductDetailsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductDetail from '../features/product/component/ProductDetails'
+import Navbar from '../features/navbar/Navbar'
+
+const ProductDetailsPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <ProductDetail/>
+    </div>
+  )
+}
+
+export default ProductDetailsPage
